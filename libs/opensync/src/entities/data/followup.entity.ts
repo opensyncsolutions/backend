6d14aa2f730bf5637570646092ea46059d3dc0f5
Bennett Benedict <bennybenester@gmail.com>
2024-03-06 10:36:23 +0300

    Add missing columns for follow up

--- libs/opensync/src/entities/data/followup.entity.ts
+++ libs/opensync/src/entities/data/followup.entity.ts
@@ -1,9 +1,9 @@
-import { Entity, JoinColumn, OneToOne } from 'typeorm';
-import { DateEntity } from '../general/date.entity';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { NameEntity } from '../general/named.entity';
 import { Enrollment } from './enrollment.entity';
 
 @Entity('followup')
-export class Followup extends DateEntity {
+export class Followup extends NameEntity {
   static plural = 'followups';
   static READ = 'READ_FOLLOWUPS';
   static ADD = 'ADD_FOLLOWUPS';
@@ -15,4 +15,16 @@ export class Followup extends DateEntity {
   })
   @JoinColumn({ name: 'enrollment' })
   enrollment: Enrollment;
+
+  @Column({ name: 'nextvisit', nullable: true })
+  nextVisit: Date;
+
+  @Column({ name: 'firstreturn', nullable: true })
+  firstReturn: Date;
+
+  @Column({ name: 'phonenumber', nullable: true })
+  phoneNumber: string;
+
+  @Column({ name: 'alternativephonenumber', nullable: true })
+  alternativePhoneNumber: string;
 }
