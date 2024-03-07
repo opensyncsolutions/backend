6904473e1246aa3f3779e9fcc364d71dc5612edd
Bennett Benedict <bennybenester@gmail.com>
2024-03-07 20:43:55 +0300

    release: Change app path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CLIENT, DB, schemaEntities } from '@app/opensync';
-import { AuthModule } from './modules/auth/auth.module';
-import { OrganisationUnitModule } from './modules/organisationunit/organisationUnit.module';
-import { join } from 'path';
+import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { EnrollmentModule } from './modules/enrollment/enrollment.module';
-import { ObjectiveModule } from './modules/objective/objective.module';
-import { FormModule } from './modules/form/form.module';
-import { PhoneModule } from './modules/phone/phone.module';
-import { NetworkModule } from './modules/network/network.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { EnrollmentModule } from './modules/enrollment/enrollment.module';
+import { FormModule } from './modules/form/form.module';
+import { NetworkModule } from './modules/network/network.module';
+import { ObjectiveModule } from './modules/objective/objective.module';
+import { OrganisationUnitModule } from './modules/organisationunit/organisationUnit.module';
+import { PhoneModule } from './modules/phone/phone.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', CLIENT),
+      rootPath: CLIENT,
     }),
     TypeOrmModule.forRoot({
       name: 'default',
       type: 'postgres',
       ...DB,
       entities: [...schemaEntities],
-      // dropSchema: true,
     }),
     AuthModule,
     OrganisationUnitModule,
