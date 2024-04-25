8bfe5244db3331d3a02275d5930b534d6947cb84
Bennett Benedict <bennybenester@gmail.com>
2024-04-25 17:35:19 +0300

    release: Update level and path on parent change

--- src/modules/organisationunit/services/organisationunit.service.ts
+++ src/modules/organisationunit/services/organisationunit.service.ts
@@ -11,4 +11,51 @@ export class OrganisationUnitService extends SharedService<OrganisationUnit> {
   ) {
     super(repository, OrganisationUnit);
   }
+
+  updateEntity = async (
+    payload: OrganisationUnit,
+  ): Promise<OrganisationUnit> => {
+    delete payload['path'];
+    const user = payload['user'];
+    const existingRecord = await this.findOneOrFailInternal({
+      id: payload['id'],
+    });
+    this.validateSystemEntity(existingRecord, payload);
+    const query = payload['query'];
+    delete payload['createdBy'];
+    delete payload['user'];
+    delete payload['query'];
+    const sanitizedPayload = await this.sanitizePayload(payload);
+    if (payload.parent && payload.parent.id) {
+      const parent = await this.findOneOrFailInternal({
+        id: payload.parent.id,
+      });
+      sanitizedPayload.level = parent.level + 1;
+      sanitizedPayload.path = `${parent.path}/${payload.id}`;
+    }
+    payload = this.repository.create({
+      ...sanitizedPayload,
+      updated: new Date().toISOString(),
+    });
+    await this.repository.save(sanitizedPayload);
+    const updatedPayload = await this.findOneOrFailInternal({
+      id: payload.id,
+      fields: query.fields,
+      filter: query.filters,
+    });
+    this.runProcess({
+      code: 'POST_UPDATE',
+      payload: { ...updatedPayload, ...payload },
+      params: { user },
+    });
+    this.runProcess({
+      code: `POST_${this.entity['plural'].toUpperCase()}_UPDATE`,
+      payload: {
+        ...updatedPayload,
+        ...payload,
+      },
+      params: { user },
+    });
+    return updatedPayload;
+  };
 }
